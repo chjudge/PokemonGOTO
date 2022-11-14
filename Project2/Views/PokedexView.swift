d461122887e779ef2c9090d61c8a61e860985c1c
chjudge <judgech19@gcc.edu>
2022-11-14 17:23:34 -0500

    Added PokemonAPI swift package and a new PokedexView

---
+++ Project2/Views/PokedexView.swift
@@ -0,0 +1,24 @@
+//
+//  PokedexView.swift
+//  Project2
+//
+//  Created by Clayton Judge on 11/14/22.
+//
+
+import SwiftUI
+import PokemonAPI
+
+struct PokedexView: View {
+    
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokedexView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokedexView()
+    }
+}
