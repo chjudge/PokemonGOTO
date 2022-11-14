6a4444b3fe27daae12a8d0892c6f4a013e12b44b
chjudge <judgech19@gcc.edu>
2022-11-13 19:49:41 -0500

    Initial Commit

---
+++ Project2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Project2
+//
+//  Created by Clayton Judge on 11/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project2/Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project2App.swift
+//  Project2
+//
+//  Created by Clayton Judge on 11/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
