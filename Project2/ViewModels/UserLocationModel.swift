fec3b1e271da491669ffb0308e502388c4169650
hestonsuorsa <SuorsaHP19@gcc.edu>
2022-11-15 20:17:06 -0500

    Map view in the works

---
+++ Project2/ViewModels/UserLocationModel.swift
@@ -0,0 +1,67 @@
+//
+//  ViewModel.swift
+//  CoreLocationApp
+//
+//  Created by Heston Suorsa on 11/11/22.
+//
+
+import Foundation
+import CoreLocation
+
+class UserLocationModel: NSObject, CLLocationManagerDelegate, ObservableObject {
+    
+    var locationManager = CLLocationManager()
+    
+    @Published var userLat: Double? = nil
+    @Published var userLon: Double? = nil
+    
+    static let shared: UserLocationModel = {
+        return UserLocationModel()
+    }()
+    
+    override init() {
+        
+        // init its parent
+        super.init()
+        
+        // create a delegate
+        locationManager.delegate = self
+        
+        // request permission to the user
+        locationManager.requestWhenInUseAuthorization()
+        
+    }
+    
+    // MARK - Location Manager Delegate Methods
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        
+        if locationManager.authorizationStatus == .authorizedAlways ||
+            locationManager.authorizationStatus == .authorizedWhenInUse {
+            
+            // We have permission
+            print("Yeah, we get the permission")
+            
+            // start geolocating the user, after we get the permission
+            locationManager.startUpdatingLocation()
+            
+        } else if locationManager.authorizationStatus == .denied {
+            
+            // We do not have permission
+            print("oh no")
+            
+        }
+        
+    }
+    
+    // capture the published geolocation by the locationManager
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations.first ?? "No location")
+        
+        if let userLocation = locations.first {
+            self.userLat = userLocation.coordinate.latitude
+            self.userLon = userLocation.coordinate.longitude
+        }
+    }
+    
+}
--- Project2/Views/MapView.swift
+++ Project2/Views/MapView.swift
@@ -1,20 +1,54 @@
 //
 //  MapView.swift
-//  Project2
+//  CoreLocationApp
 //
-//  Created by Heston Suorsa on 11/15/22.
+//  Created by Heston Suorsa on 11/11/22.
 //
 
 import SwiftUI
+import MapKit
 
-struct MapView: View {
-    var body: some View {
-        Text("Hello, Map!")
+struct MapView: UIViewRepresentable {
+    
+    var VM: UserLocationModel = UserLocationModel.shared
+    
+    var region: MKCoordinateRegion {
+        if let lat = VM.userLat, let lon = VM.userLon {
+            return MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: lon), span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001))
+        } else {
+            return MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 41.1548, longitude: -80.085), span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001))
+        }
     }
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let map = MKMapView()
+        
+        // make the user show up in the map
+        map.showsUserLocation = true // if permission doesn't get granted, doesn't work
+        map.userTrackingMode = .followWithHeading
+        
+        return map
+        
+    }
+    
+    func updateUIView(_ view: MKMapView, context: Context) {
+        
+//        view.setRegion(region, animated: true)
+
+        view.showsUserLocation = true
+        VM.locationManager.requestAlwaysAuthorization()
+        VM.locationManager.requestWhenInUseAuthorization()
+        
+        if VM.locationManager.authorizationStatus == .authorizedAlways || VM.locationManager.authorizationStatus == .authorizedWhenInUse {
+            VM.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            VM.locationManager.startUpdatingLocation()
+            let location: CLLocationCoordinate2D = VM.locationManager.location!.coordinate
+            let span = MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
+            let region = MKCoordinateRegion(center: location, span: span)
+            view.setRegion(region, animated: true)
+        }
+        
+    }
+    
 }
 
-struct MapView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapView()
-    }
-}
